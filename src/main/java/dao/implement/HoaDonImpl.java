a9c16ed629ecbf3b9f9b7e4da176b76aa584ebe9
rupinphan12345 <59106335+rupinphan12345@users.noreply.github.com>
2021-11-12 15:38:06 +0700

    Add files via upload

--- src/main/java/dao/implement/HoaDonImpl.java
+++ src/main/java/dao/implement/HoaDonImpl.java
@@ -2,12 +2,20 @@ package dao.implement;
 
 import dao.HoaDonDao;
 import entity.HoaDon;
+import entity.KhachHang;
 import util.HibernateUtil;
 
 import javax.persistence.EntityManager;
+
+import org.hibernate.Transaction;
+import org.hibernate.ogm.OgmSession;
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class HoaDonImpl extends UnicastRemoteObject implements HoaDonDao {
     private EntityManager entityManager;
@@ -18,36 +26,180 @@ public class HoaDonImpl extends UnicastRemoteObject implements HoaDonDao {
 
     @Override
     public List<HoaDon> layDanhSachHoaDon() {
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dshoadon.find({})";
+        	
+        	List<HoaDon> dsHoaDon = session.createNativeQuery(query, HoaDon.class)
+					.getResultList();
+      
+            trans.commit();
+            return dsHoaDon;
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+    	
         return null;
     }
 
     @Override
     public boolean themHoaDon(HoaDon hoaDon) {
-        return false;
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        AtomicBoolean rs = new AtomicBoolean(false);
+        
+        try {
+        	trans.begin();
+        	
+        	session.save(hoaDon);
+        	rs.set(true);
+        
+            trans.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+        return rs.get();
     }
 
     @Override
     public HoaDon timHoaDonTheoMa(String maHoaDon) {
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dshoadon.find({'_id': '"+maHoaDon+"'})";
+        	
+        	List<HoaDon> dsHoaDon = session.createNativeQuery(query, HoaDon.class)
+					.getResultList();
+      
+            trans.commit();
+            return dsHoaDon.get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
         return null;
     }
 
+//    db.dshoadon.aggregate([{$match: {$expr: {$and: [{$eq: [{$year: "$ngayTao"},2021]}, {$eq: [{$month: "$ngayTao"}, 11]}]}}}, 
+//    						{$group: {_id: null, soHoaDon: {$count: {}}}}])
     @Override
     public long laySoLuongHoaDonTheoThang(int thang) {
-        return 0;
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        AtomicInteger rs = new AtomicInteger(0);
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dshoadon.aggregate([{'$match': {'$expr': {'$and': [{'$eq': [{'$year': '$ngayTao'},2021]}, {'$eq': [{'$month': '$ngayTao'}, "+thang+"]}]}}}, {'$group': {'_id': null, 'soHoaDon': {'$count': {}}}}])";
+        	
+        	List<?> list = session.createNativeQuery(query).getResultList();
+        	
+        	for(Object obj: list) {
+				Object[] o = (Object[]) obj;
+				rs.set((int) o[1]);
+			}
+      
+            trans.commit();
+           
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+        return rs.get();
     }
 
     @Override
     public long laySoLuongHoaDonTheoNgay(int ngay, int thang) {
-        return 0;
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        AtomicInteger rs = new AtomicInteger(0);
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dshoadon.aggregate([{'$match': {'$expr': {'$and': [{'$eq': [{'$year': '$ngayTao'},2021]}, {'$eq': [{'$month': '$ngayTao'}, "+thang+"]}, {$eq: [{$dayOfMonth: \"$ngayTao\"}, "+ngay+"]}]}}}, {'$group': {'_id': null, 'soHoaDon': {'$count': {}}}}])";
+        	
+        	List<?> list = session.createNativeQuery(query).getResultList();
+        	
+        	for(Object obj: list) {
+				Object[] o = (Object[]) obj;
+				rs.set((int) o[1]);
+			}
+      
+            trans.commit();
+           
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+        return rs.get();
     }
+    
+//    db.dshoadon.aggregate([{$match: {$expr: {$and: [{$eq: [{$year: "$ngayTao"},2021]}, {$eq: [{$month: "$ngayTao"}, 11]}]}}}, {$group: {_id: null, doanhThu: {$sum: "$tongTienHoaDon"}}}])
 
     @Override
-    public long layDoanhThuTheoThang(int thang) {
-        return 0;
+    public double layDoanhThuTheoThang(int thang) {
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        AtomicReference<Double> rs = new AtomicReference<>();
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dshoadon.aggregate([{'$match': {'$expr': {'$and': [{'$eq': [{'$year': '$ngayTao'},2021]}, {'$eq': [{'$month': '$ngayTao'}, "+thang+"]}]}}}, {'$group': {'_id': null, 'doanhThu': {'$sum': '$tongTienHoaDon'}}}])";
+        	
+        	List<?> list = session.createNativeQuery(query).getResultList();
+        	
+        	for(Object obj: list) {
+				Object[] o = (Object[]) obj;
+				rs.set((double) o[1]);
+			}
+      
+            trans.commit();
+           
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+        return rs.get();
     }
+    
+//    db.dshoadon.aggregate([{$match: {$expr: {$and: [{$eq: [{$year: "$ngayTao"},2021]}, {$eq: [{$month: "$ngayTao"}, 11]}, {$eq: [{$dayOfMonth: "$ngayTao"}, 1]}]}}}, {$group: {_id: null, doanhThu: {$sum: "$tongTienHoaDon"}}}])
 
     @Override
-    public long layDoanhThuTheoNgay(int ngay, int thang) {
-        return 0;
+    public double layDoanhThuTheoNgay(int ngay, int thang) {
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        AtomicReference<Double> rs = new AtomicReference<>();
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dshoadon.aggregate([{'$match': {'$expr': {'$and': [{'$eq': [{'$year': '$ngayTao'},2021]}, {'$eq': [{'$month': '$ngayTao'}, "+thang+"]}, {'$eq': [{'$dayOfMonth': '$ngayTao'}, "+ngay+"]}]}}}, {'$group': {'_id': null, 'doanhThu': {'$sum': '$tongTienHoaDon'}}}])";
+        	
+        	List<?> list = session.createNativeQuery(query).getResultList();
+        	
+        	for(Object obj: list) {
+				Object[] o = (Object[]) obj;
+				rs.set((double) o[1]);
+			}
+      
+            trans.commit();
+           
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+        return rs.get();
     }
 }
--- src/main/java/dao/implement/KhachHangImpl.java
+++ src/main/java/dao/implement/KhachHangImpl.java
@@ -5,9 +5,15 @@ import entity.KhachHang;
 import util.HibernateUtil;
 
 import javax.persistence.EntityManager;
+
+import org.hibernate.Transaction;
+import org.hibernate.ogm.OgmSession;
+
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class KhachHangImpl extends UnicastRemoteObject implements KhachHangDao {
     private EntityManager entityManager;
@@ -18,21 +24,91 @@ public class KhachHangImpl extends UnicastRemoteObject implements KhachHangDao {
 
     @Override
     public List<KhachHang> layDanhSachKhachHang() {
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dskhachhang.find({})";
+        	
+        	List<KhachHang> dsKhachHang = session.createNativeQuery(query, KhachHang.class)
+					.getResultList();
+      
+            trans.commit();
+            return dsKhachHang;
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+    	
         return null;
     }
 
     @Override
     public boolean themKhachHang(KhachHang khachHang) {
-        return false;
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        AtomicBoolean rs = new AtomicBoolean(false);
+        
+        try {
+        	trans.begin();
+        	
+        	session.save(khachHang);
+        	rs.set(true);
+        
+            trans.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+        return rs.get();
     }
 
+//  db.dskhachhang.find({soDienThoai: '0345285471'})
     @Override
     public KhachHang timKhachHangTheoSDT(String soDienThoai) {
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dskhachhang.find({'soDienThoai': '"+soDienThoai+"'})";
+        	
+        	List<KhachHang> dsKhachHang = session.createNativeQuery(query, KhachHang.class)
+					.getResultList();
+      
+            trans.commit();
+            return dsKhachHang.get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
+    	
         return null;
     }
 
+//    db.dskhachhang.find({_id: 'KH00001'})
     @Override
     public KhachHang timKhachHangTheoMa(String maKhachHang) {
+    	OgmSession session = entityManager.unwrap(OgmSession.class);
+        Transaction trans = session.getTransaction();
+        
+        try {
+        	trans.begin();
+        	
+        	String query = "db.dskhachhang.find({'_id': '"+maKhachHang+"'})";
+        	
+        	List<KhachHang> dsKhachHang = session.createNativeQuery(query, KhachHang.class)
+					.getResultList();
+      
+            trans.commit();
+            return dsKhachHang.get(0);
+		} catch (Exception e) {
+			e.printStackTrace();
+			trans.rollback();
+		}
         return null;
     }
 }
