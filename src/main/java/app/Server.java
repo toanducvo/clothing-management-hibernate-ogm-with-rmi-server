7978ee85fc675479572e358d36c577edea438569
rupinphan12345 <59106335+rupinphan12345@users.noreply.github.com>
2021-11-12 15:39:21 +0700

    Add files via upload

--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -1,6 +1,12 @@
 package app;
 
+import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
+
+import dao.HoaDonDao;
+import dao.KhachHangDao;
+import dao.implement.HoaDonImpl;
+import dao.implement.KhachHangImpl;
 
 public class Server {
     public static void main(String[] args) {
@@ -11,7 +17,13 @@ public class Server {
         }
 
         try {
+        	HoaDonDao hoaDonDao = new HoaDonImpl();
+        	KhachHangDao khachHangDao = new KhachHangImpl();
             LocateRegistry.createRegistry(9988);
+            Naming.bind("rmi://192.168.1.9:9988/hoadonDao", hoaDonDao);
+            Naming.bind("rmi://192.168.1.9:9988/khachhangDAo", khachHangDao);
+            
+			System.out.println("Remote Object bound in RMIRegistry");
         } catch (Exception exception) {
             exception.printStackTrace();
         }
