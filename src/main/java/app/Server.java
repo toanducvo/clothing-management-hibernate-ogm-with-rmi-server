9fe61109e4e566b0af9f629e3182e69c1cac704f
rupinphan12345 <59106335+rupinphan12345@users.noreply.github.com>
2021-11-13 12:54:34 +0700

    Add files via upload

--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -5,8 +5,14 @@ import java.rmi.registry.LocateRegistry;
 
 import dao.HoaDonDao;
 import dao.KhachHangDao;
+import dao.NhanVienDao;
+import dao.SanPhamDao;
+import dao.TaiKhoanDao;
 import dao.implement.HoaDonImpl;
 import dao.implement.KhachHangImpl;
+import dao.implement.NhanVienImpl;
+import dao.implement.SanPhamImpl;
+import dao.implement.TaiKhoanImpl;
 
 public class Server {
     public static void main(String[] args) {
@@ -19,9 +25,18 @@ public class Server {
         try {
         	HoaDonDao hoaDonDao = new HoaDonImpl();
         	KhachHangDao khachHangDao = new KhachHangImpl();
+        	SanPhamDao sanPhamDao = new SanPhamImpl();
+        	NhanVienDao nhanVienDao = new NhanVienImpl();
+        	TaiKhoanDao taikhoanDao = new TaiKhoanImpl();
             LocateRegistry.createRegistry(9988);
-            Naming.bind("rmi://192.168.1.9:9988/hoadonDao", hoaDonDao);
-            Naming.bind("rmi://192.168.1.9:9988/khachhangDAo", khachHangDao);
+            Naming.bind("rmi://192.168.1.9:9988/hoaDonDao", hoaDonDao);
+            Naming.bind("rmi://192.168.1.9:9988/khachHangDao", khachHangDao);
+            Naming.bind("rmi://192.168.1.9:9988/sanPhamDao", sanPhamDao);
+            Naming.bind("rmi://192.168.1.9:9988/nhanVienDao", nhanVienDao);
+            Naming.bind("rmi://192.168.1.9:9988/taiKhoanDao", taikhoanDao);
+            
+            
+            
             
 			System.out.println("Remote Object bound in RMIRegistry");
         } catch (Exception exception) {
