92914ad6616aeb39c6549f269dc7f65a348bf82e
rupinphan12345 <59106335+rupinphan12345@users.noreply.github.com>
2021-11-25 22:22:18 +0700

    Add files via upload

--- src/main/java/app/Server.java
+++ src/main/java/app/Server.java
@@ -3,16 +3,16 @@ package app;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
-import dao.HoaDonDao;
-import dao.KhachHangDao;
-import dao.NhanVienDao;
-import dao.SanPhamDao;
-import dao.TaiKhoanDao;
-import dao.implement.HoaDonImpl;
-import dao.implement.KhachHangImpl;
-import dao.implement.NhanVienImpl;
-import dao.implement.SanPhamImpl;
-import dao.implement.TaiKhoanImpl;
+import facade.HoaDonFacade;
+import facade.KhachHangFacade;
+import facade.NhanVienFacade;
+import facade.SanPhamFacade;
+import facade.TaiKhoanFacade;
+import facade.impl.HoaDonImpl;
+import facade.impl.KhachHangImpl;
+import facade.impl.NhanVienImpl;
+import facade.impl.SanPhamImpl;
+import facade.impl.TaiKhoanImpl;
 
 public class Server {
     public static void main(String[] args) {
@@ -23,20 +23,21 @@ public class Server {
         }
 
         try {
-        	HoaDonDao hoaDonDao = new HoaDonImpl();
-        	KhachHangDao khachHangDao = new KhachHangImpl();
-        	SanPhamDao sanPhamDao = new SanPhamImpl();
-        	NhanVienDao nhanVienDao = new NhanVienImpl();
-        	TaiKhoanDao taikhoanDao = new TaiKhoanImpl();
+        	
+        	HoaDonFacade hoaDonDao = new HoaDonImpl();
+    		KhachHangFacade khachHangDao = new KhachHangImpl();
+    		NhanVienFacade nhanVienDao = new NhanVienImpl();
+    		SanPhamFacade sanPhamDao = new SanPhamImpl();
+    		TaiKhoanFacade taikhoanDao = new TaiKhoanImpl();
+    		
+        	
             LocateRegistry.createRegistry(9988);
-            Naming.bind("rmi://192.168.1.9:9988/hoaDonDao", hoaDonDao);
-            Naming.bind("rmi://192.168.1.9:9988/khachHangDao", khachHangDao);
-            Naming.bind("rmi://192.168.1.9:9988/sanPhamDao", sanPhamDao);
-            Naming.bind("rmi://192.168.1.9:9988/nhanVienDao", nhanVienDao);
-            Naming.bind("rmi://192.168.1.9:9988/taiKhoanDao", taikhoanDao);
-            
-            
-            
+            Naming.bind("rmi://192.168.1.12:9988/hoaDonDao", hoaDonDao);
+            Naming.bind("rmi://192.168.1.12:9988/khachHangDao", khachHangDao);
+            Naming.bind("rmi://192.168.1.12:9988/sanPhamDao", sanPhamDao);
+            Naming.bind("rmi://192.168.1.12:9988/nhanVienDao", nhanVienDao);
+            Naming.bind("rmi://192.168.1.12:9988/taiKhoanDao", taikhoanDao);
+
             
 			System.out.println("Remote Object bound in RMIRegistry");
         } catch (Exception exception) {
